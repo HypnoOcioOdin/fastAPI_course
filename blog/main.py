6795b7472d76033f5f5f4c9eacc372e588bae927
Tomasso Cadeo <salieri19996sklad@gmail.com>
2021-12-27 19:05:24 +0100

    response model

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,10 @@
+from typing import List
 from fastapi import FastAPI, Depends,status, Response, HTTPException
 from sqlalchemy.sql.expression import null
 from . import schemas,models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
+
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -37,23 +39,22 @@ def destroy(id, db: Session = Depends(get_db)):
 def update(id,response: Response, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"no blog with id {id} found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail = f"no blog with id {id} found")
     blog.update({models.Blog.title: request.title, models.Blog.body: request.body})
     db.commit()    
     return {"data": "updated succesfully"}
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}',status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id , response: Response ,db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"no blog with id {id} found")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"detail": f"no blog with id {id} found"}
 
     return blog
